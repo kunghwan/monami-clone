3b091a6b3573b9be274c5933e63ebfe0c1a04e2c
ykh <ysw03031@naver.com>
2025-02-24 23:30:53 +0900

    homework

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ type Person = {
   name: string;
   age: number;
 };
+
 type Pet = {
   name: string;
   age: number;
   weight: number;
   desexed: boolean;
 };
+
 const App = () => {
   const [a, setA] = useState<string>("");
   const [b, setB] = useState<number>(0);
@@ -26,12 +28,16 @@ const App = () => {
   const [people, setPeople] = useState<Person[]>([]);
 
   useEffect(() => {
+    // 상태 업데이트
     setPets((prev) => [...prev, pet1]);
     setPeople((prev) => [...prev, p1]);
+  }, [pet1, p1]);
 
+  // pets나 people 상태가 변경될 때마다 콘솔 출력
+  useEffect(() => {
     console.log("Pets Array:", pets);
     console.log("People Array:", people);
-  }, [pet1, p1]);
+  }, [pets, people]); // pets나 people이 바뀔 때마다 실행
 
   return (
     <div>
