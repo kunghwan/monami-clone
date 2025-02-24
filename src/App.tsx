cdd641252fe395a86f0c02f0cf3465f712d2a53c
kunghwan <ysw03031@naver.com>
2025-02-24 11:29:01 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import Home from "./pages/Home";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  return (
-    <>
-      <Home />
-    </>
-  );
+  // useState와 ()사이에 <> 안에 타입을 지정해줌
+  const [a, setA] = useState<number>(0);
+  const [s, setS] = useState<string>("0");
+  const [b, setB] = useState<boolean>(false);
+
+  console.log(a);
+  console.log(setA(12));
+
+  useEffect(() => {
+    setA(12);
+  }, []);
+  return <h1>sdf : {a}</h1>;
 };
 
 export default App;
