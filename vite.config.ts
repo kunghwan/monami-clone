01116ff2656a1faddda1dc1e8623f912a71b477c
kunghwan <ysw03031@naver.com>
2025-03-05 17:26:43 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
+
 import tailwindcss from "@tailwindcss/vite";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), vanillaExtractPlugin()],
 });
